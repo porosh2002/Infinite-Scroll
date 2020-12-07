a20a8e3743fd37dd4985824a34cac934d9a1fcbd
Jamil Kashem Porosh <50365984+porosh2002@users.noreply.github.com>
2020-12-08 04:04:34 +0600

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ let photosArray = [];
 // Unsplash API
 const count = 30;
 // Normally, don't store API Keys like this, but an exception made here because it is free, and the data is publicly available!
-const apiKey = 'jFgS8tteGD425f4oZfygQVaVnD6gt6GucN2yyz3xFek';
+const apiKey = '_Wmj3GF1DS8gNbFPFGalf0rBTpXyX8FweC2GbFARZhI';
 const apiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count=${count}`;
 
 // Check if all images were loaded
